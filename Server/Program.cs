fa6d9796251bed4d0b54420979f07dc67cc81954
aira.calingasan@ffufm.com <95123097+airacalins@users.noreply.github.com>
2023-05-01 10:33:14 +0800

    Added dependency injection and swagger

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,30 +1,46 @@
-﻿using HumanResourceManagement.Api.Models;
+using HumanResourceManagement.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+  options.UseSqlite(
+      builder.Configuration["ConnectionStrings:DefaultConnection"]);
 });
+
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<IJobCategoryRepository, JobCategoryRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
   app.UseWebAssemblyDebugging();
 }
-else
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-  app.UseExceptionHandler("/Error");
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -34,10 +50,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseCors("Open");
+
 app.MapControllers();
+
 app.MapFallbackToFile("index.html");
 
 app.Run();
-
